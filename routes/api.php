a3f221c9d8126eaee1b4b16faa99791554bbb872
NogaKazaha <oleg.savich2001@gmail.com>
2021-10-10 13:55:03 +0300

    Added basic auth and password reset

---
+++ routes/api.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::prefix('auth')->group(function() {
+    Route::post('/register', 'App\Http\Controllers\AuthController@register');
+    Route::post('/login', 'App\Http\Controllers\AuthController@login');
+    Route::post('/logout', 'App\Http\Controllers\AuthController@logout');
+    Route::post('/reset_password', 'App\Http\Controllers\AuthController@reset_password');
+    Route::post('/reset_password/{token}', 'App\Http\Controllers\AuthController@confirmation_token');
+});
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
